5bccd11cd3fe7a16f02317d19e7835279243f2ea
LogicCycle <israamohamed2315@gmail.com>
2025-02-14 14:28:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function sayHello() {
     let name = document.getElementById("nameInput").value;
     let output = document.getElementById("output");
     if (name) {
-        output.innerText = `Hello، ${name}!`;
+        output.innerText = `Hello, ${name}!`;
     } else {
         output.innerText = "Please Enter Your Name!";
     }
